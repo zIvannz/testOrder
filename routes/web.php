a28586936b243e96ca3b54c18516a9bb942d2564
Ivan <vladychenkoivan2407@gmail.com>
2023-02-04 14:50:18 +0200

    add layout home

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+// use App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'MainController@home')->name('home');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
